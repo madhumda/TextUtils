6c4fee8c4608365c88145ea18f2e73d158c53740
madhumda <86305117+madhumda@users.noreply.github.com>
2023-02-26 16:05:13 +0530

    Updates

---
+++ precache-manifest.9a488e6c9cd8709659bf186b379b909f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e407cf5bc9a9205ccf37",
+    "url": "/TextUtils/static/css/main.312e45c5.chunk.css"
+  },
+  {
+    "revision": "e407cf5bc9a9205ccf37",
+    "url": "/TextUtils/static/js/main.f854b18b.chunk.js"
+  },
+  {
+    "revision": "61593b1710c3a98ce72b",
+    "url": "/TextUtils/static/js/runtime~main.54cc71c8.js"
+  },
+  {
+    "revision": "693059234ba75ecc712e",
+    "url": "/TextUtils/static/js/2.8c64177d.chunk.js"
+  },
+  {
+    "revision": "9e493117a3dfb5238748",
+    "url": "/TextUtils/static/js/3.f326d3da.chunk.js"
+  },
+  {
+    "revision": "88585b6b780f93aa9508c33060367aae",
+    "url": "/TextUtils/index.html"
+  }
+];
